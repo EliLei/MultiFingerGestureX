b7b6fc2a67613aec1d592f2c8b467392450877d6
0xFantasy <fcmfantasy1999@gmail.com>
2026-05-31 12:50:46 +0800

    chore: bump version to 2.0.0

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -7,8 +7,8 @@ object Configs {
 
     const val namespace = "com.fan.edgex"
     const val applicationId = "com.fan.edgex"
-    const val versionCode = 133
-    const val versionName = "1.3.3"
+    const val versionCode = 200
+    const val versionName = "2.0.0"
 
     val javaVersion = JavaVersion.VERSION_11
     const val jvmTarget = "11"
