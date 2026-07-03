8cf87a553727869cc0aed20d5ae5fc4a1ee7e6d4
eli_lei <eli_lei@live.com>
2026-07-03 08:42:16 +0800

    chore: bump version to 0.2.14

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -7,8 +7,8 @@ object Configs {
 
     const val namespace = "com.eli.mfgx"
     const val applicationId = "com.eli.mfgx"
-    const val versionCode = 213
-    const val versionName = "0.2.13"
+    const val versionCode = 214
+    const val versionName = "0.2.14"
 
     val javaVersion = JavaVersion.VERSION_11
     const val jvmTarget = "11"
